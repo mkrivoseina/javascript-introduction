f0fd5dc228df211b3b1bdbffeb7a2ce77600fcb9
BaibaOz <78432416+yobaiba@users.noreply.github.com>
2021-03-09 21:54:38 +0200

    javascript-introduction

---
+++ task1/js/main.js
@@ -0,0 +1,5 @@
+let name = 'Marija';
+let address = 'Riga';
+let favoriteNumber = '9';
+
+console.log(`Hello, my name is ${name}, I live in ${address} and my favorite number is ${favoriteNumber}.`);
